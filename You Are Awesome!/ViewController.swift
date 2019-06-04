9ce8eb66850aa348c282bd3307ce7c11595c1a73
Bijoy Shah <bijoy.00@gmail.com>
2019-06-04 09:45:22 -0500

    2.12 (randomElement)

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -27,14 +27,18 @@ class ViewController: UIViewController {
                         "You are tremendous!",
                         "You've got the design skills of Jony Ive!",
                         "I can't wait to download your app!"]
-      
-        messageLabel.text = messages[index]
 
-        if index == messages.count - 1 {
-            index = 0
-        } else {
-            index = index + 1
-        }
+        messageLabel.text = messages.randomElement()!
+        
+//        messageLabel.text = messages[Int.random(in: 0...messages.count-1)] //this is the more complicated way to get a random element but you can also use randomElement
+        
+//        messageLabel.text = messages[index]
+//
+//        if index == messages.count - 1 {
+//            index = 0
+//        } else {
+//            index = index + 1
+//        }
         
 //        let message1 = "You Are Fantastic!!!"
 //        let message2 = "You Are Great!"
